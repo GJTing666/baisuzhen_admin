e1f8d14503f7a1b3eabc234a5956cea2c0aadb44
GJTing666 <stonetf@qq.com>
2019-11-19 17:33:00 +0800

    第二次提交 11.19

--- manage/build/webpack.base.conf.js
+++ manage/build/webpack.base.conf.js
@@ -23,10 +23,12 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json', '.css'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'components': resolve('src/components'),
+      'css': resolve('src/css')
     }
   },
   module: {
